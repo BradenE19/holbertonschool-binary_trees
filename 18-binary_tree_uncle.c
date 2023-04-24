f3ea9df26dfec6f5cd4fc55dfdcb3817713ff5c7
Braden <blearnest1995@gmail.com>
2023-04-24 11:32:31 -0700

    0x1B

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - measures the height from a noheighte in a binary tree
+ * @node: pointer to the noheighte to measure the height
+ * Return: height of the pointer, or 0 if tree is NULL
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node->parent->right == node)
+		return (node->parent->left);
+	return (node->parent->right);
+}
+
+/**
+ * binary_tree_uncle - measures the height from a noheighte in a binary tree
+ * @node: pointer to the noheighte to measure the height
+ * Return: height of the pointer, or 0 if tree is NULL
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+
+	return (binary_tree_sibling(node->parent));
+}
